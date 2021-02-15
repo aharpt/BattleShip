9d0a4873575c527a22ca0280415347bf268b4441
Aaron Harpt <a169h161@ku.edu>
2021-02-15 12:19:39 -0600

    Add columns/rows

--- dom.js
+++ dom.js
@@ -1,4 +1,7 @@
 /* when the user fires at an opponent's ship */
+const columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+const rows = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
 document.querySelector("#fire-box .btn").onclick = function() {
   let value = document.getElementById("playerGuess").value;
 
