75536ddcc9808add267349dc59802b79d3e2c4bd
Aaron Harpt <a169h161@ku.edu>
2021-02-15 12:54:40 -0600

    Add place ships functionality

--- dom.js
+++ dom.js
@@ -1,6 +1,8 @@
-/* when the user fires at an opponent's ship */
+/* available places to shoot */
 const columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 const rows = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+/* functions to verify input */
 
 let checkLetter = (value) => {
   for (let i = 0; i < columns.length; i++) {
@@ -21,6 +23,8 @@ let checkNumber = (value) => {
 
   return false;
 };
+
+/* Fire at Oppoent Ships */
 
 document.querySelector("#fire-box .btn").onclick = function() {
   let value = document.getElementById("playerGuess").value;
@@ -44,6 +48,28 @@ document.querySelector("#fire-box .btn").onclick = function() {
 
 }
 
-/* How to add a class to an element in JavaScript : https://stackoverflow.com/questions/507138/how-do-i-add-a-class-to-a-given-element */
-// document.getElementById(value).classList.remove("btn-secondary");
-// document.getElementById(value).classList.add("btn-success");
+/* Place Ships */
+
+document.querySelector("#place-box .btn").onclick = function() {
+  let value = document.getElementById("shipPlacement").value;
+  let isValidLetter = checkLetter(value);
+  let isValidNumber = checkNumber(value);
+
+  if (value.length !== 2) {
+    console.log("You must enter a string of exactly two characters that comprises a letter followed by a number (for example 'A1')");
+  }
+  else if (!isValidLetter) {
+    console.log("You entered an invalid first character. Your first character must be a letter from A-J");
+  }
+  else if (!isValidNumber) {
+    console.log("You entered an invalid second character. Your second character must be a number from 1-10");
+  }
+  else {
+    console.log("You entered a valid shot");
+    /* How to add a class to an element in JavaScript : https://stackoverflow.com/questions/507138/how-do-i-add-a-class-to-a-given-element */
+    document.getElementById(value).classList.remove("btn-secondary");
+    document.getElementById(value).classList.add("btn-success");
+  }
+
+
+}
