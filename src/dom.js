507fcdd50e4f07e90addc835ec4e2d729c744e31
Aaron Harpt <a169h161@ku.edu>
2021-02-17 16:45:39 -0600

    Work on switch statement

--- src/dom.js
+++ src/dom.js
@@ -51,8 +51,25 @@
               isValidInput = true;
               // call placeShips method to update js Board
               // switch statement based off of the value of i
-              document.getElementById(input).classList.remove("btn-secondary");
-              document.getElementById(input).classList.add("btn-success");
+
+              switch (i) {
+                case 1:
+                  document.getElementById(input).classList.remove("btn-secondary");
+                  document.getElementById(input).classList.add("btn-success");
+                  break;
+                case 2:
+                  // starting coordinate
+                  document.getElementById(input).classList.remove("btn-secondary");
+                  document.getElementById(input).classList.add("btn-success");
+                  if (orientation.toLowerCase() === "vertical") {
+                    // ending coordinate
+                    input[1] -= 1;
+                    console.log(input);
+                    document.getElementById(input).classList.remove("btn-secondary");
+                    document.getElementById(input).classList.add("btn-success");
+                  }
+                  break;
+              }
             }
           } while (!isValidInput);
 
@@ -76,31 +93,31 @@
 
   /* Place Ships should make with no button */
 
-  document.querySelector("#place-box .btn").onclick = function() {
-    let value = document.getElementById("shipPlacement").value.toUpperCase();
-    let isValidLetter = checkLetter(value);
-    let isValidNumber = checkNumber(value);
-
-    if (value.length !== 2) {
-      alert("You must enter a string of exactly two characters that comprises a letter followed by a number (for example 'A1')");
-    }
-    else if (!isValidLetter) {
-      alert("You entered an invalid first character. Your first character must be a letter from A-J");
-    }
-    else if (!isValidNumber) {
-      alert("You entered an invalid second character. Your second character must be a number from 1-10");
-    }
-    else if (document.getElementById(value).classList.contains("btn-success")) {
-      alert("There is already a ship on " + value);
-    }
-    else {
-      /* How to add a class to an element in JavaScript : https://stackoverflow.com/questions/507138/how-do-i-add-a-class-to-a-given-element */
-      document.getElementById(value).classList.remove("btn-secondary");
-      document.getElementById(value).classList.add("btn-success");
-    }
-
-    document.getElementById("shipPlacement").value = "";
-  }
+  // document.querySelector("#place-box .btn").onclick = function() {
+  //   let value = document.getElementById("shipPlacement").value.toUpperCase();
+  //   let isValidLetter = checkLetter(value);
+  //   let isValidNumber = checkNumber(value);
+  //
+  //   if (value.length !== 2) {
+  //     alert("You must enter a string of exactly two characters that comprises a letter followed by a number (for example 'A1')");
+  //   }
+  //   else if (!isValidLetter) {
+  //     alert("You entered an invalid first character. Your first character must be a letter from A-J");
+  //   }
+  //   else if (!isValidNumber) {
+  //     alert("You entered an invalid second character. Your second character must be a number from 1-10");
+  //   }
+  //   else if (document.getElementById(value).classList.contains("btn-success")) {
+  //     alert("There is already a ship on " + value);
+  //   }
+  //   else {
+  //     /* How to add a class to an element in JavaScript : https://stackoverflow.com/questions/507138/how-do-i-add-a-class-to-a-given-element */
+  //     document.getElementById(value).classList.remove("btn-secondary");
+  //     document.getElementById(value).classList.add("btn-success");
+  //   }
+  //
+  //   document.getElementById("shipPlacement").value = "";
+  // }
 
   /* Fire at Opponent Ships */
 
